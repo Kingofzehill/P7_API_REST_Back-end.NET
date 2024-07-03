6fdf62f2b5f096100a72737536cf1a11a102c514
Kingofzehill <kingofzehill@gmail.com>
2024-07-03 23:06:26 +0200

    User Input and Output Models.

---
+++ P7CreateRestApi/Models/InputModels/UserInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace P7CreateRestApi.Models.InputModel
+{
+    public class UserInputModel
+    {               
+        public string UserName { get; set; }        
+        public string Password { get; set; }        
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+}
+
+
