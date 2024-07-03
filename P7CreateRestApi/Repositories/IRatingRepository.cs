bc8b85846f4117b04cfa85f98b12eceb68ef9c8c
Kingofzehill <kingofzehill@gmail.com>
2024-07-03 12:52:30 +0200

    Rating Repository and Interface - RatingRepository for CRUD operations implementation. IRatingRepository for interface. Methods : ==> Create. ==> Delete. ==>  Get. ==> List. ==> Update. - Comment sections to all methods.

--- P7CreateRestApi/Repositories/IRatingRepository.cs
+++ P7CreateRestApi/Repositories/IRatingRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace P7CreateRestApi.Repositories
+﻿using Dot.Net.WebApi.Domain;
+
+namespace P7CreateRestApi.Repositories
 {
-    public class IRatingRepository
+    public interface IRatingRepository
     {
+        public List<Rating> List();
+        public void Create(Rating rating);
+        public Rating? Get(int id);
+        public Rating? Update(Rating rating);
+        public Rating? Delete(int id);
     }
 }
--- P7CreateRestApi/Repositories/RatingRepository.cs
+++ P7CreateRestApi/Repositories/RatingRepository.cs
@@ -1,6 +1,64 @@
-﻿namespace P7CreateRestApi.Repositories
+﻿using Dot.Net.WebApi.Domain;
+using Dot.Net.WebApi.Data;
+
+namespace P7CreateRestApi.Repositories
 {
-    public class RatingRepository
+    public class RatingRepository : IRatingRepository
     {
+        private readonly LocalDbContext _dbContext;
+        public RatingRepository(LocalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>Rating Repository Create method.</summary>  
+        /// <param name="rating">Rating DTO object</param>
+        /// <return></return> 
+        /// <remarks></remarks>
+        public void Create(Rating rating)
+        {
+            _dbContext.Ratings.Add(rating);
+            _dbContext.SaveChanges();
+        }
+        /// <summary>Rating Repository delete method.</summary>      
+        /// <return>Updated Rating.</return> 
+        /// <param name="id">Rating id to delete.</param>
+        /// <remarks></remarks>
+        public Rating? Delete(int id)
+        {
+            var rating = _dbContext.Ratings.FirstOrDefault(r => r.Id == id);
+            if (rating is not null)
+            {
+                _dbContext.Ratings.Remove(rating);
+                _dbContext.SaveChanges();
+            }
+            return rating;
+        }
+        /// <summary>Rating Repository Get method.</summary>      
+        /// <return></return> 
+        /// <param name="id">Rating id to get.</param>
+        /// <remarks></remarks>
+        public Rating? Get(int id) => _dbContext.Ratings.FirstOrDefault(r => r.Id == id);
+        /// <summary>Rating Repository List method.</summary>      
+        /// <return>List of Rating DTO objects.</return> 
+        /// <remarks></remarks>
+        public List<Rating> List() => _dbContext.Ratings.ToList();
+        /// <summary>Rating Repository Update method.</summary>      
+        /// <return>Updated Rating.</return> 
+        /// <param name="rating">Rating DTO object.</param>
+        /// <remarks></remarks>
+        public Rating? Update(Rating rating)
+        {
+            var ratingAModifier = _dbContext.Ratings.FirstOrDefault(r => r.Id == rating.Id);
+            if (ratingAModifier is not null)
+            {
+                ratingAModifier.MoodysRating = rating.MoodysRating;
+                ratingAModifier.SandPRating = rating.SandPRating;
+                ratingAModifier.FitchRating = rating.FitchRating;
+                ratingAModifier.OrderNumber = rating.OrderNumber;
+                _dbContext.SaveChanges();
+            }
+            return ratingAModifier;
+        }
     }
 }
