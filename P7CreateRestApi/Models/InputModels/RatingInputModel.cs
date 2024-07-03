036d7d37427330acfda79eb6afb70c94afa8b417
Kingofzehill <kingofzehill@gmail.com>
2024-07-03 12:33:35 +0200

    Rating POCO input and output models. - ../Models/InputModels/RatingOutputmodel.cs. Keep all fields from DTO Rating class excluding Id field. - ../Models/OutputModels/RatingOutputmodel.cs. Keep all field from POCO Rating class.

--- P7CreateRestApi/Models/InputModels/RatingInputModel.cs
+++ P7CreateRestApi/Models/InputModels/RatingInputModel.cs
@@ -1,6 +1,10 @@
-﻿namespace P7CreateRestApi.Models.InputModels
+﻿namespace P7CreateRestApi.Models.InputModel
 {
     public class RatingInputModel
     {
+        public string MoodysRating { get; set; }
+        public string SandPRating { get; set; }
+        public string FitchRating { get; set; }
+        public byte? OrderNumber { get; set; }
     }
 }
