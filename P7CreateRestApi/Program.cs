2de18be651d185a1db36d95ffacbc47b7ae72fd7
Kingofzehill <kingofzehill@gmail.com>
2024-07-05 18:25:03 +0200

    Asp.Net Core Identity Implementation # In Visual Studio Package Management Console (PMC) - Install-Package Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore -v 6.0.16 > It Provides ASP.NET Core middleware for EF Core error pages. This middleware helps to detect and diagnose errors with EF Core migrations. - Add-Migration CreateIdentitySchema -Update-database > [!NOTE] >It creates AspNet identity management tables in Database.
    # Code updates
    - LocalDbContext.cs : comment Users dbset, as replaced by identity management.
    And update class based on IdentityDbContext ==> public class LocalDbContext : IdentityDbContext<User, IdentityRole<int>, int>
    - drop-database : facultative, done for being sure of right database creation.
    - add-migration CreateIdentitySchema : prepare migration to Asp.Net Identity)
    - update database : create or update existent database.
    -  Program.cs
      -  add service Identity : builder.Services.AddIdentity<User, IdentityRole<int>>()
        .AddEntityFrameworkStores<LocalDbContext>();
      - Add User Roles management "User" and "Admin".
      - Add Authorization with "User" and "Admin" policies.

--- P7CreateRestApi/Program.cs
+++ P7CreateRestApi/Program.cs
@@ -1,5 +1,7 @@
 using Dot.Net.WebApi.Data;
+using Dot.Net.WebApi.Domain;
 using Dot.Net.WebApi.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using P7CreateRestApi.Repositories;
 using P7CreateRestApi.Services;
@@ -13,8 +15,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Asp.Net Identity to existing project 
+// https://learn.microsoft.com/fr-fr/aspnet/identity/overview/getting-started/adding-aspnet-identity-to-an-empty-or-existing-web-forms-project
 builder.Services.AddDbContext<LocalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Service ASP.NET Core Identity with User table and role based on IdentityRole.
+// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-8.0&tabs=visual-studio.
+builder.Services.AddIdentity<User, IdentityRole<int>>()
+    .AddEntityFrameworkStores<LocalDbContext>()
+    .AddDefaultTokenProviders();
+
+/*builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy =>
+    {
+        policy.RequireRole("Admin");
+        policy.RequireAuthenticatedUser();        
+    });
+
+    options.AddPolicy("User", policy =>
+    {
+        policy.RequireRole("User", "Admin");
+        policy.RequireAuthenticatedUser();        
+    });
+});*/
 
 builder.Services.AddScoped<IBidListRepository, BidListRepository>();
 builder.Services.AddScoped<IBidListService, BidListService>();
@@ -34,9 +59,42 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbcontext = scope.ServiceProvider.GetRequiredService<LocalDbContext>();
+        dbcontext.Database.EnsureCreated();
+    }
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// User roles User / Admin.
+/* using (var scope = app.Services.CreateScope())
+{
+    using var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
+    if (!await roleManager.RoleExistsAsync("User"))
+    {
+        await roleManager.CreateAsync(new IdentityRole<int> { Name = "User" });
+        await roleManager.CreateAsync(new IdentityRole<int> { Name = "Admin" });
+
+        using var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+        var admins = await userManager.GetUsersInRoleAsync("Admin");
+        if (admins.Count == 0)
+        {
+            var user = new User
+            {
+                UserName = "admin",
+                FullName = "Admin",
+                Role = "Admin",
+            };
+            var result = await userManager.CreateAsync(user, "Sy4oSfGDBWZJ8hcwOG?h$V&");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, user.Role);
+            }
+        }
+    }
+}*/
 
 app.UseHttpsRedirection();
 
